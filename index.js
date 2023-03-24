b774731f677aad34f2e3a78ef098bf014507cfaf
Vijay Giduthuri <125960600+vijaygiduthuri@users.noreply.github.com>
2023-03-25 00:37:57 +0530

    commit

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('** DEPLOYING A DOCKER NODEJS APPLICATION USING JENKINS CI/CD PIPELINE OVER THE AWS  :) **'); //write a response to the client
+  res.write('** Nodejs project  :) **'); //write a response to the client
   res.end(); //end the response
 }).listen(80); //the server object listens on port 80
