155b3f9f68760318de57a6240a032bd2145e7c75
Vijay Giduthuri <125960600+vijaygiduthuri@users.noreply.github.com>
2024-01-05 19:22:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('** Deployed a Node-js application using jenkins ci/cd and git over AWS  :) **'); //write a response to the client
+  res.write('** Deployed a Node-js application using jenkins ci/cd and git over AWS  :) ** welcome '); //write a response to the client
   res.end(); //end the response
 }).listen(80); //the server object listens on port 80
